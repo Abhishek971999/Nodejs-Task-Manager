c5460e5fef8aa0f6532b1524e8f1ef3cfa68765b
Abhishek Mogaveera <abmogaveer971999@gmail.com>
2020-06-26 00:07:50 +0530

    Password Hashed + JWT added ⚡

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-
-const User = mongoose.model('User', {
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -39,6 +40,41 @@ const User = mongoose.model('User', {
       }
     },
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
+userSchema.statics.findByCredentails = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error('Unable to Login');
+  }
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) {
+    throw new Error('Unable to Login');
+  }
+  return user;
+};
+
+userSchema.methods.generateToken = async function () {
+  const user = this;
+  const token = await jwt.sign({ _id: user.id.toString() }, 'jwttoken');
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password'))
+    user.password = await bcrypt.hash(user.password, 8);
+  next();
+});
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
--- src/routers/user.js
+++ src/routers/user.js
@@ -6,9 +6,23 @@ router.post('/users', async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    res.status(200).send(user);
+    const token = await user.generateToken();
+    res.status(200).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+
+router.post('/user/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentails(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateToken();
+    res.status(200).send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
   }
 });
 
