f665f64c07d71dc8ffd12132c2b7d4b42954139c
Abhishek Mogaveera <abmogaveer971999@gmail.com>
2020-06-28 23:32:26 +0530

    Auth Middleware added 🔒

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -10,20 +11,20 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
-    unique: true,
     lowercase: true,
     validate(value) {
       if (!validator.isEmail(value)) {
-        throw new Error('Email is Invalid');
+        throw new Error('Email is invalid');
       }
     },
   },
   password: {
     type: String,
     required: true,
-    minlength: 8,
+    minlength: 7,
     trim: true,
     validate(value) {
       if (value.toLowerCase().includes('password')) {
@@ -49,32 +50,44 @@ const userSchema = new mongoose.Schema({
     },
   ],
 });
-userSchema.statics.findByCredentails = async (email, password) => {
-  const user = await User.findOne({ email });
-  if (!user) {
-    throw new Error('Unable to Login');
-  }
-  const isValid = await bcrypt.compare(password, user.password);
-  if (!isValid) {
-    throw new Error('Unable to Login');
-  }
-  return user;
-};
 
-userSchema.methods.generateToken = async function () {
+userSchema.methods.generateAuthToken = async function () {
   const user = this;
-  const token = await jwt.sign({ _id: user.id.toString() }, 'jwttoken');
+  const token = jwt.sign({ _id: user._id.toString() }, 'jwttoken');
+
   user.tokens = user.tokens.concat({ token });
   await user.save();
+
   return token;
 };
 
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('Unable to login');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  }
+
+  return user;
+};
+
+// Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
   const user = this;
-  if (user.isModified('password'))
+
+  if (user.isModified('password')) {
     user.password = await bcrypt.hash(user.password, 8);
+  }
+
   next();
 });
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
